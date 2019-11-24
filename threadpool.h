a818e3541b7e538945f21aabe1f2a7540adb0dad
vm-iiit <virat.mishra@students.iiit.ac.in>
2019-11-24 18:02:50 +0530

    buildin

--- threadpool.h
+++ threadpool.h
@@ -7,6 +7,7 @@ struct threadpool
 {
 	int threads;
 	vector<pthread_t> thread_vector;
+	vector<bool> busy_threads;
 };
 
 threadpool create_threadpool(int num_threads = default_threads)
@@ -19,18 +20,30 @@ threadpool create_threadpool(int num_threads = default_threads)
 	{
 		pthread_t new_thread;
 		new_pool.thread_vector.push_back(new_thread);
+		new_pool.free_threads.push_back(false);
 	}
 	cout<<"address of returned pool "<<&new_pool<<endl;
 	return new_pool;
 }
 
-// void dispatch(threadpool from_me, dispatch_fn dispatch_to_here, void *arg)
-// {
+void dispatch(threadpool from_me, void* func_ptr, void *arg)
+{
+	for(int lv = 0; lv < from_me.threads; lv++)
+	{
+		if(from_me.busy_threads[lv] == false)
+			pthread_create(thread_vector[lv], NULL, *func_ptr, arg);
+	}
+}
 
-// }
+void expand_pool(threadpool& tp, int new_size)
+{
+	tp.thread_vector.resize(new_size);
+	tp.threads = new_size;
+	tp.busy_threads.resize(new_size);
+}
 
 
-// void destroy_threadpool(threadpool destroyme)
-// {
+void destroy_threadpool(threadpool destroyme)
+{
 
-// }
+}
